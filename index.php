c803a264ffd9bf7588209782d369ee5c6cdb1213
Frhsbrn <81946274+Frhsbrn@users.noreply.github.com>
2021-12-23 10:11:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,69 +10,147 @@
 
     <div style="background-color:#e5e5e5;padding:15px;text-align:center;">
         <h1>DARASYA WEDDING PLANNER</h1>
+        
     </div>
 
     <div style="overflow:auto">
         <div class="menu">
             <a href="#home">HOME</a>
             <a href="#service">SERVICE</a>
-            <a href="#aboutus">ABOUT US</a>
+            <a href="#form">FORM DETAIL</a>
             <a href="#contactus">CONTACT US</a>
         </div>
 
         <div class="main">
             <h2>DARASYA WEDDING PLANNER</h2>
+            Hello! Welcome to Darasya's Wedding Planner. 
+        We provide the wedding service you have been looking for.
+        Do have a look at our website and do contact us if 
+        you have any question. Thank you.
         </div>
 
         <div class="right">
             <h2>About</h2>
+            Darasya's services have been work since 2020.
+            We provide a good service to customer.
+            We receive a lot of good feedback.
+            For any addition service, we charg
         </div>
     </div>
-<center>
-    <form method="post" action="function.php">
-        <div class="container">
 
-            Name :
-            <input type="text" name="name" placeholder="Name" size="100" /><br><br>
+    <center>
+        <div class="slideshow-container">
 
+            <div class="mySlides fade">
+                <div class="numbertext">1 / 3</div>
+                <img src="img/decoration.jpeg" style="width:50%">
+                <div class="text">DECORATION</div>
+            </div>
 
+            <div class="mySlides fade">
+                <div class="numbertext">2 / 3</div>
+                <img src="img/makeup.jpg" style="width:50%">
+                <div class="text">BEAUTY</div>
+            </div>
 
-            Email :
-            <input type="text" placeholder="Enter Email" name="email" size="50">
-            <br><br>
+            <div class="mySlides fade">
+                <div class="numbertext">3 / 3</div>
+                <img src="img/photography.jpg" style="width:50%">
+                <div class="text">PHOTOGRAPHY</div>
+            </div>
 
-
-            <br><br>
-
-            Phone No :
-            <input type="text" placeholder="phoneNo" name="phoneNo" />
-            <br>
-            <br>
-           
-
-            <br><br>
-
-            Service Provided <br><br>
-
-            <select class=service name="service" id="service">
-                <option value="0">Select services:</option>
-                <option value="deco">Decoration</option>
-                <option value="beauty">Beauticion</option>
-                <option value="photo"> Photography</option>
-                <option value="costume">Costume</option>
-            </select>
-            <br> <br>
         </div>
-        <button type="submit" name="Submit" class="registerbtn">Register</button>
-        <button type="reset" class="resetbtn">Reset</button>
-    </form>
+        <br>
+
+        <div style="text-align:center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+    </center>
+    <center>
+        <form method="post" id="form" action="function.php">
+            <div class="container">
+
+                Name :
+                <input type="text" name="name" placeholder="Name" size="100" /><br><br>
+
+
+
+                Email :
+                <input type="text" placeholder="Enter Email" name="email" size="50">
+                <br><br>
+
+
+                <br><br>
+
+                Phone No :
+                <input type="text" placeholder="phoneNo" name="phoneNo" />
+                <br>
+                <br>
+
+
+                <br><br>
+
+                Service Provided <br><br>
+
+                <select class=service name="service" id="service">
+                    <option value="0">Select services:</option>
+                    <option value="DECORATION">Decoration</option>
+                    <option value="BEAUTICION">Beauticion</option>
+                    <option value="PHOTOGRAPHY"> Photography</option>
+                    <option value="COSTUME">Costume</option>
+                </select>
+                <br> <br>
+            </div>
+            <button type="submit" name="Submit" class="registerbtn">Register</button>
+            <button type="reset" class="resetbtn">Reset</button>
+        </form>
     </center>
 
-</body>
+    <script>
+        var slideIndex = 0;
+        showSlides();
 
-</html>
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {
+                slideIndex = 1
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+            setTimeout(showSlides, 2000); // Change image every 2 seconds
+        }
+    </script>
 
-<div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">© CREATE BY DARASYA</div>
+
+    <h3>Contact Us</h3>
+
+    <div class="container" id="contactus">
+        <form >
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+            <label for="subject">Comment</label>
+            <textarea id="subject" name="comment" placeholder="Write something.." style="height:200px"></textarea>
+
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    <div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">© CREATE BY DARASYA</div>
 
 </body>
 
